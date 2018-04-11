5ff23b2ea4fb7af4af0af361589d1aa90c003b76
Daniel Jelle Christensen <daniel@skrog.dk>
2018-04-11 10:01:59 +0200

    edits to website

--- website/index.php
+++ website/index.php
@@ -12,22 +12,29 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 
     <title>Hello, world!</title>
   </head>
-  <body>
+  <body id="top">
 
     <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"><i class="fas fa-angle-left"></i></a>
-  <a href="#">About</a>
-  <a href="#">Services</a>
-  <a href="#">Clients</a>
-  <a href="#">Contact</a>
+
+  <?php
+    //foreach ($variable as $key => $value) {
+      # code...
+    //}
+  ?>
+
+  <a href="#top"><i class="fas fa-angle-up"></i> To Top</a>
 </div>
 
-<!-- Use any element to open the sidenav -->
-<a href="javascript:void(0)" class="openbtn" onclick="openNav()"><i class="fas fa-bars"></i></a>
+<div class="sidebar">
+  <!-- Use any element to open the sidenav -->
+  <a href="javascript:void(0)" class="openbtn" onclick="openNav()"><i class="fas fa-bars"></i></a>
+</div>
 
 <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
 <div id="main">
